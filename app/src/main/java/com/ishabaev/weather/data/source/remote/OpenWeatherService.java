33abb11d84d709da1769d19e7eff4cc6b0261ada
iskandarshabaev <iskandarshabaev@gmail.com>
2016-07-20 22:00:29 +0300

    Удален символ "/" в ""data/2.5/forecast/" Из за этого символа приходили устаревшие данные

--- app/src/main/java/com/ishabaev/weather/data/source/remote/OpenWeatherService.java
+++ app/src/main/java/com/ishabaev/weather/data/source/remote/OpenWeatherService.java
@@ -13,13 +13,13 @@ import rx.Observable;
  */
 public interface OpenWeatherService {
 
-    @GET("data/2.5/weather/")
+    @GET("data/2.5/weather")
     Call<WeatherResponse> getWeather(@Query("APPID") String appid, @Query("q") String q, @Query("units") String units);
 
 
-    @GET("data/2.5/forecast/")
+    @GET("data/2.5/forecast")
     Call<Forecast> getForecast(@Query("APPID") String appid, @Query("id") int id, @Query("units") String units);
 
-    @GET("data/2.5/forecast/")
+    @GET("data/2.5/forecast")
     Observable<Forecast> getForecast2(@Query("APPID") String appid, @Query("id") int id, @Query("units") String units);
 }
