9f46929aab55ae5b188249bc0684b65346ed1489
iskandarshabaev <iskandarshabaev@gmail.com>
2016-07-22 03:30:32 +0300

    Добавлены иконки городов Иконки для города всегда рандомные

--- app/src/main/java/com/ishabaev/weather/addcity/AddCityViewAdapter.java
+++ app/src/main/java/com/ishabaev/weather/addcity/AddCityViewAdapter.java
@@ -4,23 +4,23 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ishabaev.weather.R;
 import com.ishabaev.weather.dao.OrmCity;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by ishabaev on 20.06.16.
  */
 public class AddCityViewAdapter extends RecyclerView.Adapter<AddCityViewAdapter.ViewHolder> {
 
-    public interface AddCityRecyclerViewItemListener {
-
-        void onItemClick(OrmCity city);
-    }
-
+    private Random mRandom = new Random();
+    private final static int MIN = 0;
+    private final static int MAX = 7;
     private final List<OrmCity> mCities;
     private AddCityRecyclerViewItemListener mListener;
 
@@ -63,6 +63,29 @@ public class AddCityViewAdapter extends RecyclerView.Adapter<AddCityViewAdapter.
         });
         holder.cityName.setText(mCities.get(position).getCity_name());
         holder.country.setText(mCities.get(position).getCountry());
+        holder.imageView.setImageResource(getRandomCityIcon());
+    }
+
+    private int getRandomCityIcon() {
+        int r = mRandom.nextInt(MAX - MIN + 1) + MIN;
+        switch (r) {
+            case 0:
+                return R.drawable.city0;
+            case 1:
+                return R.drawable.city1;
+            case 2:
+                return R.drawable.city2;
+            case 3:
+                return R.drawable.city3;
+            case 4:
+                return R.drawable.city4;
+            case 5:
+                return R.drawable.city5;
+            case 6:
+                return R.drawable.city6;
+            default:
+                return R.drawable.city7;
+        }
     }
 
     @Override
@@ -74,12 +97,18 @@ public class AddCityViewAdapter extends RecyclerView.Adapter<AddCityViewAdapter.
         public View view;
         public TextView cityName;
         public TextView country;
+        public ImageView imageView;
 
         public ViewHolder(View view) {
             super(view);
             this.view = view;
             this.cityName = (TextView) view.findViewById(R.id.city_name);
             this.country = (TextView) view.findViewById(R.id.country);
+            this.imageView = (ImageView) view.findViewById(R.id.ic_city);
         }
+    }
+
+    public interface AddCityRecyclerViewItemListener {
+        void onItemClick(OrmCity city);
     }
 }
